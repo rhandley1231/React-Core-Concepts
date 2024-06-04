fe52858f9a4171b599d6b7b34ac852c32ad67ceb
Ryan Handley <ryanhandley@ryans-air.mynetworksettings.com>
2024-06-04 01:56:46 -0400

    Module 1 - React Essentials: Complete.  Next up: Deep Dive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from 'react'; //A react hook
 import reactImage from './assets/react-core-concepts.png';
 import componentsImage from './assets/components.png';
-import Header from './components/Header/Header.jsx'
-import CoreConcept from './components/CoreConcept/CoreConcept.jsx'
-import TabButton from './components/TabButton/TabButton.jsx'
-import { CORE_CONCEPTS } from './data';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
+import TabButton from './components/TabButton/TabButton.jsx';
+import { CORE_CONCEPTS } from './data.js';
+import { EXAMPLES } from './data.js'
 
 function App() {
+//const stateArray = useState('Please click a button');
+const [selectedTopic, setSelectedTopic ] = useState();
+//const [Current State value,  state updating function] = useState(default state)
+
+  function handleClick(selectedContent){
+    setSelectedTopic(selectedContent);
+  }
+
+  //Alternative approach to conditional rendering using an if statement
+  let tabContent = <p>Please Select a Topic.</p>
+  if (selectedTopic){
+    tabContent = 
+    (<div id='tab-content'> 
+    <h3>{EXAMPLES[selectedTopic].title}</h3>
+    <p>{EXAMPLES[selectedTopic].description}</p>
+    <pre>
+      <code>
+        {EXAMPLES[selectedTopic].code}
+     </code>
+    </pre>
+  </div>); 
+  }
   return (
     <div>
       {/* We may call react components like this*/}
@@ -15,11 +39,18 @@ function App() {
         <section id="core-concepts">
         <h2>Core Concepts</h2>
         <ul>
-          {/* May also use these here as opposed to the spread operator
+          {/* We may use map to dynamically output list/array data*/}
+          {CORE_CONCEPTS.map((conceptItem) => 
+            (<CoreConcept key={conceptItem.title} {...conceptItem}/>)
+            )}
+
+          {/* May also use these here as opposed to the spread operator, but eww, why would we?
             title={CORE_CONCEPTS[3].title} 
             description={CORE_CONCEPTS[3].description}
             img={CORE_CONCEPTS[3].image}
              */}
+
+             {/* The map function makes this wayyyy smoother
           <CoreConcept {...CORE_CONCEPTS[0]}/>
 
           <CoreConcept {...CORE_CONCEPTS[1]}/>
@@ -27,6 +58,8 @@ function App() {
           <CoreConcept {...CORE_CONCEPTS[2]}/>
 
           <CoreConcept {...CORE_CONCEPTS[3]}/>
+        */}
+
         </ul>
         </section>
         <section id='examples'>
@@ -39,11 +72,48 @@ function App() {
             */}
 
             {/* Label approach */}
-            <TabButton label='Components'/>
-            <TabButton label='JSX'/>
-            <TabButton label='Props'/>
-            <TabButton label='State'/>
+            <TabButton 
+            selected={selectedTopic === 'components'} 
+            click={() => handleClick('components')} label='Components'
+            />
+
+            <TabButton 
+            selected={selectedTopic === 'jsx'} 
+            click={() => handleClick('jsx')} label='JSX'
+            />
+
+            <TabButton 
+            selected={selectedTopic === 'props'} 
+            click={() => handleClick('props')} label='Props'
+            />
+
+            <TabButton 
+            selected={selectedTopic === 'state'} 
+            click={() => handleClick('state')} label='State'
+            />
+
           </menu>
+          
+          {/* Conditional Rendering allows for us to output the proper html for a particular state
+          We use a ternary expression to do so*/}
+          {/*
+            { !selectedTopic ? <p>Please Select a Topic.</p> : (
+            <div id='tab-content'> 
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                  {EXAMPLES[selectedTopic].code}
+               </code>
+              </pre>
+            </div> 
+            
+            ) }
+            */ }
+            {/* Calling the Alternative approach to conditional rendering using an if statement */}
+            {tabContent}
+      
+            
         </section>
       </main>
     </div>
--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,19 +1,19 @@
 //Using the children prop, we can pass our data between the component tags
 /*
-export default function TabButton({ children }){
+export default function TabButton({ children, click }){
     return (
     <li>
-        <button>{children}</button>
+        <button onClick = {click}>{children}</button>
     </li>
     );
 }
 */
 
 //We may also pass a label and call it within the tag as a label
-export default function TabButton({ label }){
+export default function TabButton({ label, click, selected }){
     return (
     <li>
-        <button>{label}</button>
+        <button className={selected ? 'active' : undefined} onClick = {click}>{label}</button>
     </li>
     );
 }
